1ce576e3dc74c7768c6b7a139e025501d3ffc8f5
maruware <me@maruware.com>
2016-05-10 21:10:01 +0900

    'demae apply' minimum works.

--- lib/demae/cli.rb
+++ lib/demae/cli.rb
@@ -1,20 +1,50 @@
 require 'thor'
+require 'yaml'
+require 'itamae/cli'
 
 module Demae
   class CLI < Thor
-    desc 'install', 'create initial config'
+    CONFIG_DIR = 'config/demae'
+
+    def initialize(*)
+      super
+      Itamae.logger.level = ::Logger.const_get('INFO')
+      Itamae.logger.formatter.colored = true
+    end
+
+    desc 'install', 'create initial config file'
     def install
       templates_dir = File.expand_path('../templates', __FILE__)
-      config_dir = 'config/demae'
-      FileUtils.mkdir_p(config_dir)
+      FileUtils.mkdir_p(CONFIG_DIR)
       %w(staging production).each do |environment|
-        FileUtils.copy("#{templates_dir}/environment.yml", "#{config_dir}/#{environment}.yml")
+        FileUtils.copy("#{templates_dir}/environment.yml", config_path(environment))
       end
     end
 
-    desc 'apply', 'apply itamae recipes'
+    desc 'apply', 'apply itamae recipes to environment'
     def apply(environment)
-      # TODO: impl
+      config = YAML.load_file(config_path(environment))
+      config.each do |server|
+        server['hosts'].each do |host|
+          options = {
+            host: host,
+            vagrant: false,
+            ask_password: false,
+            sudo: true
+          }
+          o = server.reject { |k, _| %w(hosts recipes).include? k }
+          o = o.each_with_object({}) { |(k, v), memo| memo[k.to_sym] = v }
+          options.merge!(o)
+
+          runner = Itamae::Runner.run(server['recipes'], :ssh, options)
+        end
+      end
+    end
+
+    private
+
+    def config_path(environment)
+      "#{CONFIG_DIR}/#{environment}.yml"
     end
   end
 end
